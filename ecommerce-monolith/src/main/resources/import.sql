2a1fcc76e83988c0010ba185840a8102bdb9713b
Vinicius A. Santos <vinicius.vas.ti@gmail.com>
2024-04-05 08:55:23 -0300

    Initializing the db data

---
+++ ecommerce-monolith/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO productCategory (id, name) VALUES (1, 'Books');
+INSERT INTO productCategory (id, name) VALUES (2, 'Electronics');
+INSERT INTO productCategory (id, name) VALUES (3, 'Clothing');
+INSERT INTO productCategory (id, name) VALUES (4, 'Home & Kitchen');
