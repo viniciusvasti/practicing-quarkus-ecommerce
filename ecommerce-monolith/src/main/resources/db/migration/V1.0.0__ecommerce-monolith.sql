e3ab9095b20c0e5d6b808f4e6027adfb789d195f
Vinicius A. Santos <vinicius.vas.ti@gmail.com>
2024-04-11 19:36:40 -0300

    Add Flyway migration and update application properties

--- ecommerce-monolith/src/main/resources/db/migration/V1.0.0__ecommerce-monolith.sql
+++ ecommerce-monolith/src/main/resources/db/migration/V1.0.0__ecommerce-monolith.sql
@@ -1,10 +1,50 @@
--- Inserting product categories
+CREATE sequence product_inventory_SEQ START WITH 1 increment by 50;
+
+CREATE sequence product_price_SEQ START WITH 1 increment by 50;
+
+CREATE sequence Product_SEQ START WITH 1 increment by 50;
+
+CREATE sequence ProductCategory_SEQ START WITH 1 increment by 50;
+
+CREATE TABLE Product (
+    category_id bigint,
+    id bigint NOT NULL,
+    description varchar(255),
+    name varchar(255),
+    sku varchar(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE product_inventory (
+    stockUnits integer NOT NULL,
+    id bigint NOT NULL,
+    sku varchar(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE product_price (
+    price numeric(38, 2),
+    id bigint NOT NULL,
+    sku varchar(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE ProductCategory (
+    id bigint NOT NULL,
+    name varchar(255),
+    PRIMARY KEY (id)
+);
+
+ALTER TABLE
+    IF EXISTS Product
+ADD
+    CONSTRAINT FKnkccxbwt1qp2wrqbvb2tfcch4 FOREIGN KEY (category_id) REFERENCES ProductCategory;
+
+-- TODO: Figure out how to run those inserts only for dev and test profiles
 INSERT INTO productCategory (id, name) VALUES (nextval('productcategory_seq'), 'Books');
 INSERT INTO productCategory (id, name) VALUES (nextval('productcategory_seq'), 'Electronics');
 INSERT INTO productCategory (id, name) VALUES (nextval('productcategory_seq'), 'Clothing');
 INSERT INTO productCategory (id, name) VALUES (nextval('productcategory_seq'), 'Home & Kitchen');
-
--- Inserting products
 INSERT INTO product (id, sku, name, description, category_id) VALUES (nextval('product_seq'), '00000001', 'The Pragmatic Programmer', 'Your journey to mastery', 1);
 INSERT INTO product (id, sku, name, description, category_id) VALUES (nextval('product_seq'), '00000002', 'Clean Code', 'A Handbook of Agile Software Craftsmanship', 1);
 INSERT INTO product (id, sku, name, description, category_id) VALUES (nextval('product_seq'), '00000003', 'MacBook Pro', 'The ultimate pro notebook', 51);
@@ -16,8 +56,6 @@ INSERT INTO product (id, sku, name, description, category_id) VALUES (nextval('p
 INSERT INTO product (id, sku, name, description, category_id) VALUES (nextval('product_seq'), '00000009', 'Samsung Galaxy S21 Ultra', 'The ultimate smartphone', 51);
 INSERT INTO product (id, sku, name, description, category_id) VALUES (nextval('product_seq'), '00000010', 'Samsung Galaxy Watch 3', 'The most advanced smartwatch', 51);
 INSERT INTO product (id, sku, name, description, category_id) VALUES (nextval('product_seq'), '00000011', 'Samsung Galaxy Buds Pro', 'The ultimate earbuds', 51);
-
--- Inserting products prices
 INSERT INTO product_price (id, sku, price) VALUES (nextval('product_price_seq'), '00000001', 29.99);
 INSERT INTO product_price (id, sku, price) VALUES (nextval('product_price_seq'), '00000002', 37.99);
 INSERT INTO product_price (id, sku, price) VALUES (nextval('product_price_seq'), '00000003', 1299.99);
@@ -26,8 +64,6 @@ INSERT INTO product_price (id, sku, price) VALUES (nextval('product_price_seq'),
 INSERT INTO product_price (id, sku, price) VALUES (nextval('product_price_seq'), '00000006', 150.00);
 INSERT INTO product_price (id, sku, price) VALUES (nextval('product_price_seq'), '00000007', 79.95);
 INSERT INTO product_price (id, sku, price) VALUES (nextval('product_price_seq'), '00000008', 79.99);
-
--- Inserting products inventory
 INSERT INTO product_inventory (id, sku, stockunits) VALUES (nextval('product_inventory_seq'), '00000001', 5);
 INSERT INTO product_inventory (id, sku, stockunits) VALUES (nextval('product_inventory_seq'), '00000002', 10);
 INSERT INTO product_inventory (id, sku, stockunits) VALUES (nextval('product_inventory_seq'), '00000003', 3);
