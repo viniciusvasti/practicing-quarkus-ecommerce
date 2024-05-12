bc0f851e0dcc4c5e06850744ddf258a11e6a5065
Vinicius A. Santos <vinicius.vas.ti@gmail.com>
2024-05-12 13:56:14 -0300

    Update product name example in UpdateProductDetailsDTO

--- ecommerce-monolith/src/main/java/org/vas/product/details/presentation/dtos/UpdateProductDetailsDTO.java
+++ ecommerce-monolith/src/main/java/org/vas/product/details/presentation/dtos/UpdateProductDetailsDTO.java
@@ -5,7 +5,7 @@ import org.eclipse.microprofile.openapi.annotations.media.Schema;
 @Schema(name = "UpdateProduct", description = "A product in the catalog")
 public record UpdateProductDetailsDTO(
         @Schema(required = true) Long id,
-        @Schema(required = true, example = "12345678") String name,
+        @Schema(required = true, example = "Product name") String name,
         @Schema(required = true, example = "Product Description") String description,
         @Schema(required = true, example = "1") Long categoryId) {
 }
